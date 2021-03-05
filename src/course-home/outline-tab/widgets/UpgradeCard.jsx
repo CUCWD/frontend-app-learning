e97ec9484c13bc3fcc452acef3e25ef5d0cfd0c5
Emma Green <emma.green.software@gmail.com>
2021-03-05 10:41:49 -0500

    static expiration box

--- src/course-home/outline-tab/widgets/UpgradeCard.jsx
+++ src/course-home/outline-tab/widgets/UpgradeCard.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { faCheckCircle as farCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 import { sendTrackEvent, sendTrackingLogEvent } from '@edx/frontend-platform/analytics';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { getConfig } from '@edx/frontend-platform';
 import { Button } from '@edx/paragon';
 
 import messages from '../messages';
@@ -49,7 +53,40 @@ function UpgradeCard({ courseId, intl, onLearnMore }) {
   };
 
   return (
-    <section className="mb-4 p-3 outline-sidebar-upgrade-card">
+    <section className="mb-4 card">
+        <h2 className="h5 m-3" id="outline-sidebar-upgrade-header">{intl.formatMessage(messages.upsellFirstPurchaseDiscount)}</h2>
+        <div className="p-3 upsell-warning">6 days left</div>
+        <div className="pt-3">
+          <ul className="fa-ul">
+            <li>
+              <span className="fa-li"><FontAwesomeIcon icon={farCheckCircle} /></span> 
+              Earn a <a className="inline-link-underline" href={`${getConfig().MARKETING_SITE_BASE_URL}/verified-certificate`}>verified certificate</a> of completion to showcase on your resume
+            </li>
+            <li>
+              <span className="fa-li"><FontAwesomeIcon icon={farCheckCircle} /></span> 
+              Unlock your access to all course activities, including <span className="font-weight-bold">graded assignments</span>
+            </li>
+            <li>
+              <span className="fa-li"><FontAwesomeIcon icon={farCheckCircle} /></span> 
+              <span className="font-weight-bold">Full access</span> to course content and materials, even after the course ends
+            </li>
+            <li>
+              <span className="fa-li"><FontAwesomeIcon icon={farCheckCircle} /></span> 
+              Support our <span className="font-weight-bold">non-profit mission</span> at edX
+            </li>
+          </ul>
+        </div>
+        <UpgradeButton
+                offer={offer}
+                onClick={logClick}
+                verifiedMode={verifiedMode}
+                className="ml-3 mr-3 mb-3"
+              />
+      <div className="bg-light p-3 text-center discount-info">
+      Use code <span className="font-weight-bold">EDXWELCOME</span> at checkout
+      </div>
+    </section>
+    /* <section className="mb-4 p-3 outline-sidebar-upgrade-card">
       <h2 className="h4" id="outline-sidebar-upgrade-header">{intl.formatMessage(messages.upgradeTitle)}</h2>
       <div className="row w-100 m-0">
         <div className="col-6 col-md-12 col-lg-3 col-xl-4 p-0 text-md-center text-lg-left">
@@ -83,7 +120,7 @@ function UpgradeCard({ courseId, intl, onLearnMore }) {
           </div>
         </div>
       </div>
-    </section>
+    </section> */
   );
 }
 
